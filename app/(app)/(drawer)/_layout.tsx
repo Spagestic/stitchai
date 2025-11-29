8f00284c8d935b864bae2d2ecac0bd69acbac4a2
spagestic <steelmax666@gmail.com>
2025-11-29 15:27:43 +0800

    increase swipe edge width for drawer navigation

--- app/(app)/(drawer)/_layout.tsx
+++ app/(app)/(drawer)/_layout.tsx
@@ -42,7 +42,7 @@ function DrawerLayout() {
         },
         overlayColor: 'transparent',
         swipeEnabled: true,
-        swipeEdgeWidth: 50,
+        swipeEdgeWidth: 450,
         swipeMinDistance: 5,
       }}>
       <Drawer.Screen
